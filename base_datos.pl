2bf28a7ef1c17f853ea8449a1b14943450a5295d
vladioan <vladioan@vladioan-HP.(none)>
2015-02-19 13:49:42 +0100

    Añadiendo relaciones

---
+++ base_datos.pl
@@ -0,0 +1,8 @@
+chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry,]).
+pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma]).
+ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy]).
+triste([paul,derek,louis,sam,billy,henry,sarah]).
+gafas([albert,]).
+ojos_azules().
+
+es_chico(X):- chico(L), member(X,L).
