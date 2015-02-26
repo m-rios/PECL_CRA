4315ab0b502b7923d5776fff45c9a0da0a84ba90
vladioan <vladioan@vladioan-HP.(none)>
2015-02-26 12:41:22 +0100

    Fin base de conocimiento

--- base_datos.pl
+++ base_datos.pl
@@ -2,7 +2,7 @@ chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthon
 pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma]).
 ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy]).
 triste([paul,derek,louis,sam,billy,henry,sarah]).
-gafas([albert,]).
-ojos_azules().
+gafas([albert,michael,charles,anthony,natalie,sabrina]).
+ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
 
 es_chico(X):- chico(L), member(X,L).
