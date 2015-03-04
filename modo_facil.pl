ce75c4caee040ab2a4dee486cd2623599a2a406f
Mario Rios <mrm994@gmail.com>
2015-03-04 13:36:04 +0100

    implementada asignación de personajes

--- modo_facil.pl
+++ modo_facil.pl
@@ -8,16 +8,10 @@ ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
 acciones_maquina(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','ropa_roja','ropa_verde','ojos_azules','ojos_marrones']).
 
 personas([albert, paul, tom, derek, richard, louis, michael, charles, sam, steve, will, anthony, billy, henry, tiffany, natalie, roxanne, sarah, sabrina, cindy, emma]).
-personas_maquina([]).
-personas_jugador([]).
 test([derek]).
 
 personaje_maquina([]).
 personaje_jugador([]).
-
-asignar_maquina:- personas(L),random_select(X,L,L1), personaje_maquina(M), 
-    append(M,X), personas_maquina(M2), append(M2,L1).
-asignar_jugador:- personas(L), random_select(X,L,L1), personaje_jugador(J), append(J,X).
 
 do_accion(A):- A = 'chico', writeln('personaje chico').
 do_accion(A):-A = 'chica', writeln('personaje chica').
@@ -30,13 +24,21 @@ do_accion(A):-A = 'ropa_roja', writeln('personaje ropa_roja').
 do_accion(A):-A = 'ropa_verde', writeln('personaje ropa_verde').
 do_accion(A):-A = 'ojos_azules', writeln('personaje ojos_azules').
 do_accion(A):-A = 'ojos_marrones', writeln('personaje ojos_marrones').
-do_accion(A):- A = 'salir', writeln('saliendo').
 do_accion(A):- writeln('cualidad no reconocida :3').
 
 leer_entrada(X):- write('escoge cualidades: '), read(X).
 
 jugar_maquina(Acc,NAcc):- random_select(X,Acc,NAcc), do_accion(X).
 
-jugar:- acciones_maquina(L), jugar(L).
+jugar:- 
+    %recuperar lista de acciones disponibles para la maquina.
+    acciones_maquina(L),
+    %Pm -> personaje maquina, Lb-> lista inicial, La-> Lb-Pm
+    personas(Lbm),
+    random_select(Pm,Lbm,Lam),
+    random_select(Pj, Lam, Laj),
+    %ejecutar consola de juego
+    jugar(L, Pm, Pj).
 
-jugar(Acc):- leer_entrada(X), do_accion(X), X\='salir' ,write('la maquina ha escogido: '), jugar_maquina(Acc,NAcc), jugar(NAcc).
+jugar(Acc, Pm, Pj):- write('tu personaje es: '), writeln(Pj), leer_entrada(X), do_accion(X),
+    write('la maquina ha escogido: '), jugar_maquina(Acc,NAcc), jugar(NAcc).
