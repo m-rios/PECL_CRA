4a5956668796621eb686c1ebbb6d84aa3096bad1
Mario Rios <mrm994@gmail.com>
2015-03-06 17:38:24 +0100

    solo falta descartar

--- base_datos.pl
+++ base_datos.pl
@@ -1,8 +1,11 @@
-chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry,]).
+es_chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry]).
 pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma]).
 ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy]).
-triste([paul,derek,louis,sam,billy,henry,sarah]).
+esta_triste([paul,derek,louis,sam,billy,henry,sarah]).
 gafas([albert,michael,charles,anthony,natalie,sabrina]).
 ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
 
-es_chico(X):- chico(L), member(X,L).
+acciones(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','ropa_roja','ropa_verde','ojos_azules','ojos_marrones']).
+
+personas([albert, paul, tom, derek, richard, louis, michael, charles, sam, steve, will, anthony, billy, henry, tiffany, natalie, roxanne, sarah, sabrina, cindy, emma]).
+test([derek]).
--- modo_facil.pl
+++ modo_facil.pl
@@ -5,49 +5,70 @@ esta_triste([paul,derek,louis,sam,billy,henry,sarah]).
 gafas([albert,michael,charles,anthony,natalie,sabrina]).
 ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
 
-acciones_maquina(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','ropa_roja','ropa_verde','ojos_azules','ojos_marrones']).
+preguntas(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','ropa_roja','ropa_verde','ojos_azules','ojos_marrones']).
 
 personas([albert, paul, tom, derek, richard, louis, michael, charles, sam, steve, will, anthony, billy, henry, tiffany, natalie, roxanne, sarah, sabrina, cindy, emma]).
 test([derek]).
 
-personaje_maquina([]).
-personaje_jugador([]).
+%hacer_pregunta(A, Personaje, Lista_personajes):- A = 'chico', es_chico(Chicos), member(P,Chicos),descartarchicas.
+%hacer_pregunta(A, P, L):- A = 'chico', .
+hacer_pregunta(P, Sol):-P=Sol, write('Has acertado, mi personaje era '), writeln(Sol).
+hacer_pregunta(P, Sol):-P = 'chico', writeln('personaje chico').
+hacer_pregunta(P, Sol):-P = 'chica', writeln('personaje chica').
+hacer_pregunta(P, Sol):-P = 'gafas', writeln('personaje gafas').
+hacer_pregunta(P, Sol):-P = 'pelo_rubio', writeln('personaje pelo_rubio').
+hacer_pregunta(P, Sol):-P = 'pelo_negro', writeln('personaje pelo_negro').
+hacer_pregunta(P, Sol):-P = 'feliz', writeln('personaje feliz').
+hacer_pregunta(P, Sol):-P = 'triste', writeln('personaje triste').
+hacer_pregunta(P, Sol):-P = 'ropa_roja', writeln('personaje ropa_roja').
+hacer_pregunta(P, Sol):-P = 'ropa_verde', writeln('personaje ropa_verde').
+hacer_pregunta(P, Sol):-P = 'ojos_azules', writeln('personaje ojos_azules').
+hacer_pregunta(P, Sol):-P = 'ojos_marrones', writeln('personaje ojos_marrones').
+hacer_pregunta(P, Sol):- writeln('cualidad no reconocida :3').
 
-%do_accion(A, Personaje, Lista_personajes):- A = 'chico', es_chico(Chicos), member(P,Chicos),descartarchicas.
-%do_accion(A, P, L):- A = 'chico', .
-do_accion(A):-A = 'chico', writeln('personaje chico').
-do_accion(A):-A = 'chica', writeln('personaje chica').
-do_accion(A):-A = 'gafas', writeln('personaje gafas').
-do_accion(A):-A = 'pelo_rubio', writeln('personaje pelo_rubio').
-do_accion(A):-A = 'pelo_negro', writeln('personaje pelo_negro').
-do_accion(A):-A = 'feliz', writeln('personaje feliz').
-do_accion(A):-A = 'triste', writeln('personaje triste').
-do_accion(A):-A = 'ropa_roja', writeln('personaje ropa_roja').
-do_accion(A):-A = 'ropa_verde', writeln('personaje ropa_verde').
-do_accion(A):-A = 'ojos_azules', writeln('personaje ojos_azules').
-do_accion(A):-A = 'ojos_marrones', writeln('personaje ojos_marrones').
-do_accion(A):- writeln('cualidad no reconocida :3').
+leer_pregunta(X):- write('haz tu pregunta: '), read(X).
+%pregunta optima: si tengo igual de una caracteristica q de la contraria, elijo esa, porque me divide en 2 el problema
+jugar_maquina(Preg,NPreg,Personaje_jugador):- random_select(X,Preg,NPreg), hacer_pregunta(X,Personaje_jugador).
 
-leer_entrada(X):- write('escoge cualidades: '), read(X).
+imprimir_candidatos([Candidato|Candidatos]):- 
+    write(Candidato), write(': '),print_genero(Candidato),write(', '),print_pelo(Candidato),write(', '),
+    print_estado(Candidato),write(', '), print_gafas(Candidato),write(', '),
+    print_ojos(Candidato),writeln(''), imprimir_candidatos(Candidatos).
+imprimir_candidatos([]):-write('').
+print_genero(Candidato):- es_chico(Chicos) ,member(Candidato, Chicos), write('Chico');write('Chica').
+print_pelo(Candidato):- pelo_rubio(Rubios), member(Candidato, Rubios), write('Pelo rubio');write('Pelo negro').
+print_ropa(Candidato):- ropa_roja(Rojos), member(Candidato, Rojos), write('Ropa roja');write('Ropa verde').
+print_estado(Candidato):-esta_triste(Tristes), member(Candidato, Tristes), write('triste');write('feliz').
+print_gafas(Candidato):-gafas(Gafotas), member(Candidato, Gafotas), write('lleva gafas');write('sin gafas').
+print_ojos(Candidato):-ojos_azules(OjosAzules), member(Candidato, OjosAzules), write('Ojos azules');write('Ojos marrones').
 
-jugar_maquina(Acc,NAcc):- random_select(X,Acc,NAcc), do_accion(X).
 
 facil:-     
-    %recuperar lista de acciones disponibles para la maquina.
-    acciones_maquina(Acciones),
-    %Pm -> personaje maquina, Lb-> lista inicial, La-> Lb-Pm
+    %recuperar todas las acciones disponibles
+    preguntas(Preguntas),
     personas(Personas),
     random_select(Personaje_maquina,Personas,Personas2),
     random_select(Personaje_jugador, Personas2, _),
     select(Personaje_maquina, Personas, Candidatos_maquina),
     select(Personaje_jugador, Personas, Candidatos_jugador),
+    writeln(Candidatos_jugador),
     write('tu personaje es: '), writeln(Personaje_jugador),
+    write('DEBUG!! El personaje de la maquina es: '), writeln(Personaje_maquina),
     %ejecutar consola de juego
-    jugar(Acciones, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
+    jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
 
-jugar(Acciones, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina):-      
-    leer_entrada(X), do_accion(X),
-    write('la maquina ha escogido: '), jugar_maquina(Acciones,NAcc), jugar(NAcc, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
+jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina):-      
+    writeln('-------------------------------------------------------------------'),
+    writeln('Tus candidatos a elegir son:'),
+    imprimir_candidatos(Candidatos_jugador),
+    %ejecutar accion jugador
+    write('Las preguntas que puedes hacer son: '),
+    preguntas(L),
+    writeln(L),
+    leer_pregunta(X), hacer_pregunta(X,Personaje_maquina),
+    %ejecutar accion maquina
+    write('la maquina ha escogido: '), jugar_maquina(Preguntas,NPreg,Personaje_jugador), 
+    jugar(NPreg, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
 
 
 
