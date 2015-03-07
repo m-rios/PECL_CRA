59c9d108e280e54e8a83770036238c426f3083ff
vladioan <vladioan@vladioan-HP.(none)>
2015-03-07 12:07:24 +0100

    Hacer pregunta, resta y eliminar

--- base_datos.pl
+++ base_datos.pl
@@ -1,11 +1,9 @@
-es_chico([albert,paul,tom,derek,richard,louis,michael,charles,sam,steve,will,anthony,billy,henry]).
-pelo_rubio([paul,michael,sam,will,anthony,billy,natalie,roxanne,emma]).
-ropa_roja([albert,paul,richard,louis,sam,steve,anthony,henry,natalie,sarah,cindy]).
-esta_triste([paul,derek,louis,sam,billy,henry,sarah]).
-gafas([albert,michael,charles,anthony,natalie,sabrina]).
-ojos_azules([albert,richard,louis,sam,will,billy,natalie,roxanne,sabrina]).
+resta([X,Candidatos],Aeliminar, Resto):-
+	guardar(X,Aeliminar,Resto,Resto_aux),
+	resta(Candidatos,Aeliminar,Resto).
+resta([],Aeliminar,Resto).
 
-acciones(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','ropa_roja','ropa_verde','ojos_azules','ojos_marrones']).
-
-personas([albert, paul, tom, derek, richard, louis, michael, charles, sam, steve, will, anthony, billy, henry, tiffany, natalie, roxanne, sarah, sabrina, cindy, emma]).
-test([derek]).
+guardar(X,Aeliminar,Resto,Resto_aux):-
+	union(Resto,[],Resto_aux),
+	not(member(X,Aeliminar)),
+	insertar(X,Resto_aux,Resto).
