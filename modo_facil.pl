3f272e8cb3089758fd9fc5aae0b11d9cf0e1d4e9
Mario Rios <mrm994@gmail.com>
2015-03-09 00:18:20 +0100

    TODO FUNCIONA!
    he arreglado todos los errores (CREO).
    TODO:
    -modo avanzado
    -mejorar los mensajes de feedback
    -comentar, refactorizar y limpiar

--- modo_facil.pl
+++ modo_facil.pl
@@ -9,8 +9,6 @@ preguntas(['chico','chica','gafas','pelo_rubio','pelo_negro','feliz','triste','r
 
 personas([albert, paul, tom, derek, richard, louis, michael, charles, sam, steve, will, anthony, billy, henry, tiffany, natalie, roxanne, sarah, sabrina, cindy, emma]).
 
-%hacer_pregunta(A, Personaje, Lista_personajes):- A = 'chico', es_chico(Chicos), member(P,Chicos),descartarchicas.
-%hacer_pregunta(A, P, L):- A = 'chico', .
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-P=Sol, write('Has acertado, mi personaje era '), writeln(Sol).
 
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
@@ -33,8 +31,7 @@ hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
     %si la pregunta es pelo rubio, y el personaje es rubio
     P = 'pelo_rubio', pelo_rubio(Rubios), member(Sol,Rubios),intersection(Candidatos,Rubios,N_candidatos); 
     %si el personaje no es rubio
-    P='pelo_rubio',pelo_rubio(Rubios),personas(Personas),
-    resta(Personas,Rubios,Morenos),resta(Candidatos,Rubios,N_candidatos).
+    P='pelo_rubio',pelo_rubio(Rubios),resta(Candidatos,Rubios,N_candidatos).
 
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
     %si la pregunta es pelo negro y el candidato es moreno, nos quedamos con los no rubios
@@ -58,7 +55,7 @@ hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
     P = 'ropa_roja',ropa_roja(RopaRoja),resta(Candidatos,RopaRoja,N_candidatos).
 
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
-    P = 'ropa_verde', ropa_roja(RopaRoja), member(Sol,RopaRoja), resta(Candidatos,RopaRoja,N_candidatos);
+    P = 'ropa_verde', ropa_roja(RopaRoja), not(member(Sol,RopaRoja)), resta(Candidatos,RopaRoja,N_candidatos);
     P='ropa_verde',ropa_roja(RopaRoja),intersection(Candidatos,RopaRoja,N_candidatos).
 
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
@@ -66,10 +63,10 @@ hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
     P='ojos_azules',ojos_azules(OjosAzules),resta(Candidatos,OjosAzules,N_candidatos).
 
 hacer_pregunta(P,Candidatos,Sol,N_candidatos):-
-    P ='ojos_verdes', ojos_azules(OjosAzules),member(Sol,OjosAzules),resta(Candidatos,OjosAzules,N_candidatos);
-    P='ojos_verdes',ojos_azules(OjosAzules),intersection(Candidatos,OjosAzules,N_candidatos).
+    P ='ojos_marrones', ojos_azules(OjosAzules),not(member(Sol,OjosAzules)),resta(Candidatos,OjosAzules,N_candidatos);
+    P='ojos_marrones',ojos_azules(OjosAzules),intersection(Candidatos,OjosAzules,N_candidatos).
 
-hacer_pregunta(P,Candidatos,Sol,N_candidatos):- writeln('cualidad no reconocida :3').
+hacer_pregunta(P,Candidatos,Sol,N_candidatos):- writeln('pregunta no reconocida :3'), append(Candidatos,[],N_candidatos).
 
 resta(L,[],Resultado):-append(L,[],Resultado).	
 resta(Candidatos,[X|Aeliminar],Resultado):-
@@ -82,12 +79,14 @@ elimina(X,[H|T],[H|T1]):- elimina(X,T,T1).
 
 leer_pregunta(X):- write('haz tu pregunta: '), read(X).
 %pregunta optima: si tengo igual de una caracteristica q de la contraria, elijo esa, porque me divide en 2 el problema
-jugar_maquina(Preg,NPreg,Personaje_jugador):- random_select(X,Preg,NPreg), hacer_pregunta(X,Personaje_jugador).
+jugar_maquina(Preg,NPreg,Candidatos_maquina,Personaje_jugador,N_candidatos_maquina):- 
+    write('Ahora me toca a mi, '),random_select(P,Preg,NPreg), write(P), write(' ?'),
+    hacer_pregunta(P,Candidatos_maquina,Personaje_jugador,N_candidatos_maquina).
 
 imprimir_candidatos([Candidato|Candidatos]):- 
     write(Candidato), write(': '),print_genero(Candidato),write(', '),print_pelo(Candidato),write(', '),
     print_estado(Candidato),write(', '), print_gafas(Candidato),write(', '),
-    print_ojos(Candidato),writeln(''), imprimir_candidatos(Candidatos).
+    print_ojos(Candidato),write(', '),print_ropa(Candidato),writeln('') ,imprimir_candidatos(Candidatos).
 imprimir_candidatos([]):-write('').
 print_genero(Candidato):- es_chico(Chicos) ,member(Candidato, Chicos), write('Chico');write('Chica').
 print_pelo(Candidato):- pelo_rubio(Rubios), member(Candidato, Rubios), write('Pelo rubio');write('Pelo negro').
@@ -95,10 +94,6 @@ print_ropa(Candidato):- ropa_roja(Rojos), member(Candidato, Rojos), write('Ropa 
 print_estado(Candidato):-esta_triste(Tristes), member(Candidato, Tristes), write('triste');write('feliz').
 print_gafas(Candidato):-gafas(Gafotas), member(Candidato, Gafotas), write('lleva gafas');write('sin gafas').
 print_ojos(Candidato):-ojos_azules(OjosAzules), member(Candidato, OjosAzules), write('Ojos azules');write('Ojos marrones').
-
-%descartar(Candidatos, Descartados):-.fa
-
-
 
 facil:-     
     %recuperar todas las acciones disponibles
@@ -112,25 +107,22 @@ facil:-
     write('tu personaje es: '), writeln(Personaje_jugador),
     write('DEBUG!! El personaje de la maquina es: '), writeln(Personaje_maquina),
     %ejecutar consola de juego
-    jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, emma).
+    jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
 
-jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina):-      
-length(Candidatos_jugador, L),L=1,write('Tu ganas, mi personaje es '),writeln(Personaje_maquina);
-length(Candidatos_maquina,L),L=1, write('Yo gano, tu personaje es '), writeln(Personaje_jugador).
+jugar(Preguntas, [C1|Candidatos_jugador], [C2|Candidatos_maquina], Personaje_jugador, Personaje_maquina):-      
+    length(Candidatos_jugador, L),L=0,write('Tu ganas, mi personaje es '),writeln(C1);
+    length(Candidatos_maquina,L),L=0, write('Yo gano, tu personaje es '),writeln(C2).
 jugar(Preguntas, Candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina):-      
     writeln('-------------------------------------------------------------------'),
     writeln('Tus candidatos a elegir son:'),
     imprimir_candidatos(Candidatos_jugador),
     %ejecutar accion jugador
     write('Las preguntas que puedes hacer son: '),
-    preguntas(L),
-    writeln(L),
-    leer_pregunta(X), hacer_pregunta(X,Candidatos_jugador,Personaje_maquina,N_candidatos_jugador),
-    %ejecutar accion maquina
-    %write('la maquina ha escogido: '), 
-    %random_select(X,Preguntas,NPreg),
-    %hacer_pregunta(X,Candidatos_maquina,Personaje_jugador,N_candidatos_maquina),
-    jugar(NPreg, N_candidatos_jugador, Candidatos_maquina, Personaje_jugador, Personaje_maquina).
-
-
-
+    preguntas(L), writeln(L),
+    leer_pregunta(P), hacer_pregunta(P,Candidatos_jugador,Personaje_maquina,N_candidatos_jugador),
+    %turno maquina:
+    %jugar_maquina(Preguntas,NPreg,Candidatos_maquina,Personaje_jugador,N_candidatos_maquina),
+    write('Ahora me toca a mi, '),random_select(Preg,Preguntas,NPreg), write(Preg), writeln(' ?'),
+    hacer_pregunta(Preg,Candidatos_maquina,Personaje_jugador,N_Candidatos_maquina),
+    write('mmm, dudo entre '),length(Candidatos_maquina,N),write(N),writeln(' posibilidades'),
+    jugar(NPreg, N_candidatos_jugador, N_Candidatos_maquina, Personaje_jugador, Personaje_maquina).
