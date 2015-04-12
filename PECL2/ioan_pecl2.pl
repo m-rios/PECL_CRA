2c2d1b288030d4caf8fc922e25684a7a379b894b
Ioan <ioan1994ro@gmail.com>
2015-04-12 20:48:57 +0200

    todo eso nano

--- PECL2/ioan_pecl2.pl
+++ PECL2/ioan_pecl2.pl
@@ -1,27 +1,42 @@
 % Reglas gramaticales
-consult(draw).
+
+test:- consult(draw), oracionCompuesta(X,[el,gato,que,es,gris,ama,a,maria,que,es,negro],[]), draw(X). 
 
 oracion(o(GN,GV)) --> g_nominal(GN), g_verbal(GV).
+oracion(o(GV)) --> g_verbal(GV).
 
-orsubordinada(or(C,GV)) --> conjunciones(C), g_verbal(GV).
+oracionSimple(X) --> oracion(X).
 
-g_nominal(gn(N)) --> nombre(N).
-g_nominal(gn(D,N)) --> determinante(D), nombre(N).
+oracionCompuesta(X) --> orsubordinada(X).
+oracionCompuesta(X) --> orcoordinada(X).
+
+nexo(X) --> adverbio(X).
+nexo(X) --> conjunciones(X).
+
+orcoordinada((oc(OR1, PR, OR2))) --> oracion(OR1), nexo(PR), oracion(OR2).
+
+orsubordinada(or(OR)) --> oracion(OR).
+
+g_preposicional(gp(PR,GN)) --> preposicion(PR), g_nominal(GN).
+
 g_nominal(gn(D,N,ADJ)) --> determinante(D), nombre(N), g_adjetival(ADJ).
 g_nominal(gn(N,ADJ)) --> nombre(N), g_adjetival(ADJ).
+g_nominal(gn(N,ADJ)) --> nombrePropio(N), g_adjetival(ADJ).
+g_nominal(gn(D,N)) --> determinante(D), nombre(N).
+g_nominal(gn(N)) --> nombrePropio(N).
 
-g_verbal(gv(V)) --> verbo(V).
-g_verbal(gv(V,GN)) --> verbo(V), g_nominal(GN).
-g_verbal(gv(V,P,GN)) --> verbo(V), preposicion(P) ,g_nominal(GN).
+g_verbal(gv(V,GPR)) --> verbo(V), g_preposicional(GPR).
 g_verbal(gv(V,ADJ)) --> verbo(V), g_adjetival(ADJ).
-
+g_verbal(gv(V,GN)) --> verbo(V), g_nominal(GN).
+g_verbal(gv(V)) --> verbo(V).
 
 g_adjetival(gadj(ADJ)) --> adjetivo(ADJ).
-g_adjetival(gadj(OR)) --> orsubordinada(OR).
+g_adjetival(gadj(CJ,OR)) --> conjunciones(CJ), orsubordinada(OR).
 
-g_adverbial(gadv(A)) --> adverbio(ADV).
-g_adverbial(gadj(OR)) --> orsubordinada(OR).
+g_adverbial(gadv(ADV)) --> adverbio(ADV).
+g_adverbial(gadj(CJ,OR)) --> conjunciones(CJ),orsubordinada(OR).
 %Diccionario
+ 		
 
 %determinantes
 determinante(det(X)) --> [X],{det(X)}.
@@ -42,7 +57,7 @@ n(ratones).
 n(raton).
 n(alumno).
 
-nombrePropio(n(X)) --> [X],{n(X)}.
+nombrePropio(np(X)) --> [X],{np(X)}.
 np(juan).
 np(maria).
 
@@ -58,6 +73,7 @@ v(era).
 %conjunciones
 conjunciones(conj(X)) --> [X],{conj(X)}.
 conj(que).
+conj(y).
 
 %preposiciones
 preposicion(prep(X)) --> [X],{prep(X)}.
@@ -74,5 +90,9 @@ adj(pequeno).
 
 %adverbios
 adverbio(adv(X)) --> [X],{adv(X)}.
+adv(mientras).
+
+
+
 
 %pronombres
