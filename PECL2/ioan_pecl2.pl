ec30023424a3b14a9025f09fa6e0af2485f95094
Ioan <ioan1994ro@gmail.com>
2015-04-13 20:30:18 +0200

    perfecto tío, perfecto. Me encanta. Ole ole que hoy es jueves. Grupo preposicional, bien definido, de momento algunas frases coordinadas van guay aunque ya veremos despues el resto.

--- PECL2/ioan_pecl2.pl
+++ PECL2/ioan_pecl2.pl
@@ -6,6 +6,8 @@ countV([X|L],P):-countV(L,P1), v(X), P is P1+1.
 countV([_|L],P):-countV(L,P).
 countV([],0).
 
+analizar(ORACION):-consult(draw),countV(ORACION,V), V = 1, oracion(X,ORACION,[]), draw(X).
+analizar(ORACION):- oracionCompuesta(X,ORACION,[]), draw(X).
 
 oracion(o(GN,GV)) --> g_nominal(GN), g_verbal(GV).
 oracion(o(GV)) --> g_verbal(GV).
@@ -15,21 +17,23 @@ oracionSimple(X) --> oracion(X).
 oracionCompuesta(X) --> orsubordinada(X).
 oracionCompuesta(X) --> orcoordinada(X).
 
-nexo(X) --> adverbio(X).
-nexo(X) --> conjunciones(X).
-
 orcoordinada((oc(OR1, PR, OR2))) --> oracion(OR1), nexo(PR), oracion(OR2).
+orcoordinada((oc(OR1, PR1, OR2, PR2, OR3))) --> oracion(OR1), nexo(PR1), oracion(OR2), nexo(PR2), oracion(OR3).
 
 orsubordinada(or(OR)) --> oracion(OR).
 
 g_preposicional(gp(PR,GN)) --> preposicion(PR), g_nominal(GN).
 
 g_nominal(gn(D,N,ADJ)) --> determinante(D), nombre(N), g_adjetival(ADJ).
+g_nominal(gn(D,N,PREP)) --> determinante(D), nombre(N), g_preposicional(PREP).
 g_nominal(gn(N,ADJ)) --> nombre(N), g_adjetival(ADJ).
 g_nominal(gn(N,ADJ)) --> nombrePropio(N), g_adjetival(ADJ).
+g_nominal(gn(N1, PR, N2)) --> nombrePropio(N1), nexo(PR), nombrePropio(N2).
 g_nominal(gn(D,N)) --> determinante(D), nombre(N).
 g_nominal(gn(N)) --> nombrePropio(N).
 
+g_verbal(gv(V1,PR,V2, ADJ)) --> verbo(V1), nexo(PR), verbo(V2), g_adjetival(ADJ).
+g_verbal(gv(V1,PR,V2)) --> verbo(V1), nexo(PR), verbo(V2).
 g_verbal(gv(V,GPR)) --> verbo(V), g_preposicional(GPR).
 g_verbal(gv(V,ADJ)) --> verbo(V), g_adjetival(ADJ).
 g_verbal(gv(V,GN)) --> verbo(V), g_nominal(GN).
@@ -40,6 +44,7 @@ g_adjetival(gadj(CJ,OR)) --> conjunciones(CJ), orsubordinada(OR).
 
 g_adverbial(gadv(ADV)) --> adverbio(ADV).
 g_adverbial(gadj(CJ,OR)) --> conjunciones(CJ),orsubordinada(OR).
+
 %Diccionario
  		
 
@@ -58,6 +63,7 @@ n(manzanas).
 n(universidad).
 n(manzana).
 n(gato).
+n(canario).
 n(ratones).
 n(raton).
 n(alumno).
@@ -65,6 +71,7 @@ n(alumno).
 nombrePropio(np(X)) --> [X],{np(X)}.
 np(juan).
 np(maria).
+np(irene).
 
 %verbos
 verbo(v(X)) --> [X],{v(X)}.
@@ -72,18 +79,25 @@ v(ama).
 v(come).
 v(cazo).
 v(estudia).
+v(canta).
 v(es).
+v(salta).
+v(sonrie).
 v(era).
 
 %conjunciones
 conjunciones(conj(X)) --> [X],{conj(X)}.
 conj(que).
-conj(y).
+conj(de).
+
+nexo(nx(X)) --> [X],{nx(X)}.
+nx(y).	
 
 %preposiciones
 preposicion(prep(X)) --> [X],{prep(X)}.
 prep(a).
 prep(en).
+prep(de).
 
 %adjetivos
 adjetivo(adj(X)) --> [X],{adj(X)}.
@@ -91,6 +105,7 @@ adj(roja).
 adj(negro).
 adj(grande).
 adj(gris).
+adj(alegre).
 adj(pequeno).	
 
 %adverbios
