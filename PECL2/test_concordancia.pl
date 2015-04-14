f0d20f5127a60dab9f737c8d122351d1e74dfe5e
Mario Rios <mrm994@gmail.com>
2015-04-14 19:58:13 +0200

    concordancia verbal

--- PECL2/test_concordancia.pl
+++ PECL2/test_concordancia.pl
@@ -9,8 +9,7 @@ countV([],0).
 analizar(ORACION):-consult(draw),countV(ORACION,V), V = 1, oracion(X,ORACION,[]), draw(X).
 analizar(ORACION):- oracionCompuesta(X,ORACION,[]), draw(X).
 
-oracion(o(GN,GV)) --> g_nominal(GN), g_verbal(GV).
-oracion(o(GV)) --> g_verbal(GV).
+oracion(o(GN,GV)) --> g_nominal(GN,Gen,Num,Per), g_verbal(GV,Gen,Num,Per).
 
 oracionSimple(X) --> oracion(X).
 
@@ -20,43 +19,53 @@ oracionCompuesta(X) --> orcoordinada(X).
 orcoordinada((oc(OR1, PR, OR2))) --> oracion(OR1), nexo(PR), oracion(OR2).
 orcoordinada((oc(OR1, PR1, OR2, PR2, OR3))) --> oracion(OR1), nexo(PR1), oracion(OR2), nexo(PR2), oracion(OR3).
 
-g_nominal(gn(D,N,ADJ)) --> determinante(D,C), nombre(N,C), g_adjetival(ADJ,C).
-g_nominal(gn(N,ADJ,C)) --> nombre(N,C), g_adjetival(ADJ,C).
+g_nominal(gn(D,N,ADJ),Gen,Num,Per) --> determinante(D,Gen,Num,Per), nombre(N,Gen,Num,Per), g_adjetival(ADJ,Gen,Num,Per).
+g_nominal(gn(N,ADJ),Gen,Num,Per) --> nombre(N,Gen,Num,Per), g_adjetival(ADJ,Gen,Num,Per).
 
-g_adjetival(gadj(ADJ),C) --> adjetivo(ADJ,C).
+g_verbal(gv(V),Gen,Num,Per) --> verbo(V,Gen,Num,Per).
+
+g_adjetival(gadj(ADJ),Gen,Num,Per) --> adjetivo(ADJ,Gen,Num,Per).
 
 %Diccionario
-        
+%verbos
+verbo(v(X),Gen,Num,Per) --> [X],{v(X,Gen,Num,Per)}.
+v(ama,_,s,3).
+v(come,_,s,3).
+v(cazo,_,s,3).
+v(estudia,_,s,3).
+v(canta,_,s,3).
+v(es,_,s,3).
+v(son,_,p,3).
+v(salta,_,s,3).
+v(sonrie,_,s,3).
+v(era,_,s,3).        
 
 %determinantes
-determinante(det(X),C) --> [X],{det(X,C)}.
-det(el,ms).
-det(la,fs).
-det(un,ms).
-det(una,fs).
+determinante(det(X),Gen,Num,Per) --> [X],{det(X,Gen,Num,Per)}.
+det(el,m,s,_).
+det(la,f,s,_).
+det(un,m,s,_).
+det(una,f,s,_).
 
 %nombres
-nombre(n(X),C) --> [X],{n(X,C)}.
-n(hombre,ms).
-n(mujer,fs).
-n(manzanas,fp).
-n(universidad,fs).
-n(manzana,fs).
-n(gato,ms).
-n(canario,ms).
-n(ratones,mp).
-n(raton,ms).
-n(alumno,ms).
+nombre(n(X),Gen,Num,Per) --> [X],{n(X,Gen,Num,Per)}.
+n(hombre,m,s,_).
+n(mujer,f,s,_).
+n(manzanas,f,p,_).
+n(universidad,f,s,_).
+n(manzana,f,s,_).
+n(gato,m,s,_).
+n(canario,m,s,_).
+n(ratones,m,p,_).
+n(raton,m,s,_).
+n(alumno,m,s,_).
 
 %adjetivos
-adjetivo(adj(X),C) --> [X],{adj(X,C)}.
-adj(roja,fs). 
-adj(rojo,ms).
-adj(negro,ms).
-adj(grande,ms).
-adj(grande,fs).
-adj(gris,ms).
-adj(gris,fs).
-adj(alegre,ms).
-adj(alegre,fs).
-adj(pequeno,ms).   
+adjetivo(adj(X),Gen,Num,Per) --> [X],{adj(X,Gen,Num,Per)}.
+adj(roja,f,s,_). 
+adj(rojo,m,s,_).
+adj(negro,m,s,_).
+adj(grande,_,s,_).
+adj(gris,_,s,_).
+adj(alegre,_,s,_).
+adj(pequeno,m,s,_).   
